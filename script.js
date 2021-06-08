98e33ff6137957a2285081987042c9509c5b70ed
erichard9000 <78449655+erichard9000@users.noreply.github.com>
2021-06-08 00:14:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 function compute()
 {
-    var principal= document.getElementById("principal").value;
-    if (principal <= 0 || principal == "") {
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var currentYear = new Date().getFullYear();
+    var futureYear = currentYear + years;
+
+    if (isNaN(principal) || principal <= 0) {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
-        return false;}
-    var rate=document.getElementById("rate").value;
-    var years= document.getElementById("years").value;
-    var interest =(principal * years * rate)/100;
-    var date =new Date()
-    var year= date.getFullYear()+parseInt(years);
-        
-    var result = document.getElementById("result");
-    output = "If you deposit <mark>" + principal + "</mark>,<br>"
-            + "at an interest rate of <mark>" + rate + "</mark>%.<br>"
-            + "You will receive an amount of <mark>" + interest + "</mark>,<br>"
-            + "in the year <mark>" + year + "</mark>";
-    result.innerHTML = output;
-function updateRate(){
-    var ratenum= document.getElementById("rate").value;
-    var output = document.getElementById("rate_val");
-    var percentage = ratenum.value + "%";
-    output.innerHTML = percentage;
+    } else {
+        document.getElementById("result").innerHTML =
+            "            If you deposit <mark>" + principal + "</mark>,<br>\n" +
+            "            at an interest rate of <mark>" + rate + "%</mark>.<br>\n" +
+            "            You will receive an amount of <mark>" + interest + "</mark>,<br>\n" +
+            "            in the year <mark>" + futureYear + "</mark>";
+    }
+}
+
+function updateRate() {
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value + "%";
 
     function validateAmount() {
         var principal = document.getElementById("principal").value;
